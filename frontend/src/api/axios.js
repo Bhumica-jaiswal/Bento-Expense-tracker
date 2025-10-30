7c27ada1ee63049851683bb7c386496fcd925457
Bhumica-jaiswal <bhumicajaiswal@gmail.com>
2025-10-30 19:25:49 +0530

    fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-baseURL: `${import.meta.env.VITE_API_URL}/api`,
+baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Interceptor to add the auth token to every request
